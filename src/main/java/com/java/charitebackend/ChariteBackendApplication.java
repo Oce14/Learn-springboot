270acf269d26989258aaa4cf6978a90686d4bd99
Oce14 <oceane.le-quellec@etu.univ-grenoble-alpes.fr>
2024-04-30 14:50:40 +0200

    Add initial configuration files, basic dependencies and main folders for the project structure.

---
+++ src/main/java/com/java/charitebackend/ChariteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.java.charitebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChariteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChariteBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/charitebackend/ChariteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.charitebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChariteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
